bb1631af9cfe163eaab646f071ee445b667e4a3e
LeandroKastilho <119023278+LeandroKastilho@users.noreply.github.com>
2022-12-17 01:20:00 -0300

    Atualização de ex

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 120
+console.log(`A velocidade do seu carro é de ${vel} Km/h.`)
+if (vel > 60) {
+    console.log(`Você passou o limite de Velocidade de você foi Multado`)
+}
+
+console.log(`Dirija sempre usando o cinto de segurança`)
