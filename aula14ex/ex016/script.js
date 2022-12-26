aa9b28084c38ac161577764939bd9107ef124705
LeandroKastilho <119023278+LeandroKastilho@users.noreply.github.com>
2022-12-25 21:25:55 -0300

    ex 16 criado

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,23 +1,16 @@
-function carregar(){
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    
-    msg.innerHTML = `Agora são ${hora} horas!`
-    msg.style.color  = `white`
-    if (hora >= 0 && hora < 12){
-        msg.innerText += ' Bom Dia'
-        img.src = `manha.png`
-        document.body.style.background = `#f5d041`
-    }else if(hora >= 12 && hora < 18){
-        msg.innerText += ' Boa Tarde'
-        img.src = `tarde.png`
-        document.body.style.background = `#de823f`
+function tabuada(){
+    var num = window.document.getElementById('txtn')
+    var tab = window.document.getElementById('seltab')
+    if(num.value.length == 0){
+        window.alert('Por Favor, Digite um Número! ')
     }else{
-        msg.innerText += ' Boa Noite'
-        img.src = `noite.png`
-        document.body.style.background = `#052943`
+        var n = Number(num.value)
+        tab.innerHTML = ''
+        for(c = 1; c <= 10;c++) {
+            var item = document.createElement('option')
+            item.text = ` ${n} x ${c} = ${n*c} `
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
     }
-
 }
