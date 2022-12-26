cdf02950764679f0fdec46265a855e8f02df771f
LeandroKastilho <119023278+LeandroKastilho@users.noreply.github.com>
2022-12-26 01:23:52 -0300

    atualização dos ex 17

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,23 +1,17 @@
-function carregar(){
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
+function tabuada(){
+    let num = [txtn, seltab]
+    if(num[0].value.length == 0){
+        window.alert('[ERRO] Por favor, digite um número.')
+    }else {
+        let n = Number(num[0].value)
+        num[1].innerHTML = ' '
     
-    msg.innerHTML = `Agora são ${hora} horas!`
-    msg.style.color  = `white`
-    if (hora >= 0 && hora < 12){
-        msg.innerText += ' Bom Dia'
-        img.src = `manha.png`
-        document.body.style.background = `#f5d041`
-    }else if(hora >= 12 && hora < 18){
-        msg.innerText += ' Boa Tarde'
-        img.src = `tarde.png`
-        document.body.style.background = `#de823f`
-    }else{
-        msg.innerText += ' Boa Noite'
-        img.src = `noite.png`
-        document.body.style.background = `#052943`
+    for(c = 1;c <= 10;c++){
+        var item = document.createElement('option')
+        item.text = ` ${n} x ${c} = ${n * c} `
+        item.value =`num[1] ${c}`
+        num[1].appendChild(item)
     }
+  }
 
 }
